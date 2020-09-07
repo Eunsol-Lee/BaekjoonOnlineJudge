cd4b1c26d38745d0c91d8edfa9ffc470fd2e2ef5
Eunsol <eunsol@medibloc.org>
2020-09-08 07:19:37 +0900

    2020/09/08 12 problems ~1003, stage1

---
+++ 1003.py
@@ -0,0 +1,9 @@
+T = int(input())
+
+fib = [[1, 0], [0, 1]]
+for i in range(2, 50):
+  fib.append([fib[i - 2][0] + fib[i - 1][0], fib[i - 2][1] + fib[i - 1][1]])
+
+for _ in range(T):
+  N = int(input())
+  print (fib[N][0], fib[N][1])
